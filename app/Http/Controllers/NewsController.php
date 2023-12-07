29b6af5bc4c091813e6b4a17e35db0d243fbd0c2
DELANNOY Tanguy <74651888+Bleidwyn@users.noreply.github.com>
2023-12-08 00:16:31 +0100

    add: Controller for news from Bon Pote

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function getNews(){
+        $rssUrl = "https://bonpote.com/feed/"
+        if ($rssUrl !== false)
+        {
+            $rss = simplexml_load_file($rssUrl);
+            if ($rss !== false)
+            {
+                $items = $rss->channel->item;
+                return view('actu')->with('items', $items);
+            }
+            else
+            {
+                return "Erreur lors de l\'analyse du flux RSS.";
+            }
+        }
+        else
+        {
+            return "Erreur lors de la récupération du flux RSS.";
+        }
+    }
+}
