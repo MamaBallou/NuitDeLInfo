d918212e2785d06bdc6450f2ada19adf9620eb68
sbriffaud <simon.briffaud@orange.fr>
2023-12-08 01:41:43 +0100

    Maj : accueil

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -9,16 +9,6 @@ class AccueilController extends Controller {
     private const ABILITY = 'accueil';
 
     public function index() {
-        $response = Http::get('https://data-transitions2050.ademe.fr/data-fair/api/v1/catalog/datasets?topics=TJWq9NFA4igXtdTLEYRa0,Iiyy_fvwjxDnhG9hu3ZVF,osPW_q87OEXxcllo_gAj4&select=id');
-
-        $table_ids = [];
-        if($response->successful()) {
-            $results = $response->json();
-            foreach($results['results'] as $result) {
-                array_push($table_ids, $result['id']);
-            }
-        }
-
-        return view(self::ABILITY.'.index', compact('table_ids'));
+        return view(self::ABILITY.'.index');
     }
 }
--- resources/views/accueil/index.blade.php
+++ resources/views/accueil/index.blade.php
@@ -13,25 +13,9 @@
 
         z-index: -1;
     }
-
-    .actu-card {
-        width: 5em;
-
-        padding: 0;
-
-        border: none;
-    }
 </style>
 @endsection
 
 @section('content')
 {{-- <img src="{{ asset('img/background-2.png') }}" alt="logo" id="accueil-background-image"> --}}
-
-@include('components.carousel', [
-    'table_ids' => $table_ids,
-])
-
-<div class="row d-flex flex-wrap">
-</div>
-
 @endsection
