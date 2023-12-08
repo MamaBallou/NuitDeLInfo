eb1b014aa00728ce438baf11cf707232ee2ca5bb
Xynoss <74651875+Xynoss@users.noreply.github.com>
2023-12-08 07:44:42 +0100

    add : btn escape game

--- resources/views/accueil/index.blade.php
+++ resources/views/accueil/index.blade.php
@@ -13,9 +13,39 @@
 
         z-index: -1;
     }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        width: 100%;
+    }
+
+    .blur-background {
+        background-color: rgba(255, 255, 255, 0.2);
+        backdrop-filter: blur(5px);
+        display : block;
+        margin: 3%;
+        padding: 2%;
+        border-radius: 10px;
+    }
+
+    .escapegame_btn{
+        background-color: #49704f;
+        backdrop-filter: blur(5px);
+        justify-content: center;
+        align-items: center;
+    }
 </style>
 @endsection
 
 @section('content')
 <img src="{{ asset('img/logo_solair.png') }}" alt="logo" id="accueil-background-image">
+<div class="container">
+    <div class="blur-background">
+        <h1 class="text-center">Bienvenue sur le site de Sol'Air</h1>
+        <p class="text-center">Faisons de notre planète un endroit plus propre et plus sain.</p>
+    </div>
+        <a href="{{ route('escape_game.index') }}" class="btn escapegame_btn">Jouer à l'escape game</a>
+</div>
 @endsection
